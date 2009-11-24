fa900e49936d82cbb3effa409f15ef1643cfbe6d
bond-forever@yandex.ru <bond-forever@yandex.ru@6423568c-d02b-11de-83be-cfe0592a616e>
2009-11-24 12:32:47 +0000

    Переместил TestFrame в пакет по умолчанию.

--- trunk/src/CatalogDB/src/TestFrame.java
+++ trunk/src/CatalogDB/src/TestFrame.java
@@ -1,13 +1,14 @@
-package al.catalog;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class TestFrame extends JFrame {
 
@@ -22,14 +23,28 @@ public class TestFrame extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 
-		String[] data = { "Windows XP", "Windows Vista", "Mac OS", "Chrome OS",
-				"Ubuntu", "Linux", "Unix", "Solaris", "Windows 98", "Windows 95", "Windows 2000", "Windows 2003"};
-		
-		JList list = new JList(data);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		
+		String[] data = { "Windows XP", "Windows Vista", "Mac OS",
+				"Chrome OS", "Ubuntu", "Linux", "Unix", "Solaris",
+				"Windows 98", "Windows 95", "Windows 2000",
+				"Windows 2003" };
+
+		final JList list = new JList(data);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
 		panel.add(new JScrollPane(list), BorderLayout.CENTER);
-		
+
+		final JLabel label = new JLabel(" ");
+
+		panel.add(label, BorderLayout.SOUTH);
+
+		list.addListSelectionListener(
+				new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent e) {
+						Object element = list.getSelectedValue();
+						label.setText(element.toString());
+					}
+				});
+
 		getContentPane().add(panel);
 
 		setPreferredSize(new Dimension(250, 200));
