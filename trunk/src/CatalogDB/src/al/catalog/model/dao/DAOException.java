afe597dd722a74472bf723c2b09239a4b15b8c1f
bond-forever@yandex.ru <bond-forever@yandex.ru@6423568c-d02b-11de-83be-cfe0592a616e>
2009-11-25 14:45:33 +0000

    Добавлены комментарии к классу DAOException.

--- trunk/src/CatalogDB/src/al/catalog/model/dao/DAOException.java
+++ trunk/src/CatalogDB/src/al/catalog/model/dao/DAOException.java
@@ -8,10 +8,23 @@ package al.catalog.model.dao;
  */
 public class DAOException extends Exception {
 
+	/**
+	 * Создает новый объект <b>DAOException</b> с текстовым сообщением об ошибке.
+	 * 
+	 * @param message - <b>String</b> строка с текстом сообщения об ошибке.
+	 */
 	public DAOException(String message) {
 		super(message);		
 	}
 	
+	/**
+	 * Создает новый объект <b>DAOException</b> на основе другой исключительной
+	 * ситуации.
+	 * 
+	 * @param e -
+	 *            <b>Exception</b>, на основе которой создается новый объект
+	 *            <b>DAOException</b>.
+	 */
 	public DAOException(Exception e) {
 		super(e);
 	}
