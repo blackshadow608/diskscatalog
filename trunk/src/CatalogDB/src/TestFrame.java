7a605b41c1ccc3843a2bf352ff18808ddc29ed45
bond-forever@yandex.ru <bond-forever@yandex.ru@6423568c-d02b-11de-83be-cfe0592a616e>
2009-12-18 13:34:50 +0000

    Реализован пример для JCheckBox.

--- trunk/src/CatalogDB/src/TestFrame.java
+++ trunk/src/CatalogDB/src/TestFrame.java
@@ -1,18 +1,23 @@
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.Icon;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.UIManager;
 
 public class TestFrame extends JFrame {
+
+	public static String TEST_TEXT = "Test text";
+
+	private JLabel label;
+
+	private JCheckBox italicBox = new JCheckBox("Italic");
+	private JCheckBox boldBox = new JCheckBox("Bold");
+	private JCheckBox underlineBox = new JCheckBox("Underline");
 
 	public TestFrame() {
 		super("Test frame");
@@ -25,33 +30,72 @@ public class TestFrame extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 
-		String[] data = { "Windows XP", "Windows Vista", "Mac OS",
-				"Chrome OS", "Ubuntu", "Linux", "Unix", "Solaris",
-				"Windows 98", "Windows 95", "Windows 2000",
-				"Windows 2003" };
+		label = new JLabel(getText());
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setFont(new Font("Calibri", Font.PLAIN, 30));
+		panel.add(label, BorderLayout.CENTER);
 
-		final JList list = new JList(data);
-		
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
-		list.setCellRenderer(new DefaultListCellRenderer() {
-			public Component getListCellRendererComponent(JList list,
-					Object value, int index, boolean isSelected,
-					boolean cellHasFocus) {				
-				Component component = super.getListCellRendererComponent(
-						list, value, index, isSelected, cellHasFocus);
-				JLabel label = (JLabel) component;
-				Icon icon = UIManager.getIcon("Tree.closedIcon");
-				label.setIcon(icon);
-				return label;
+		JPanel boxesPanel = new JPanel();
+
+		boxesPanel.add(italicBox);
+
+		italicBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateText();
 			}
 		});
 
-		panel.add(new JScrollPane(list), BorderLayout.CENTER);
+		boxesPanel.add(boldBox);
 
+		boldBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateText();
+			}
+		});
+
+		boxesPanel.add(underlineBox);
+
+		underlineBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateText();
+			}
+		});
+
+		panel.add(boxesPanel, BorderLayout.SOUTH);
 		getContentPane().add(panel);
-
 		setPreferredSize(new Dimension(250, 200));
+	}
+
+	public void updateText() {
+		label.setText(getText());
+	}
+
+	private String getText() {
+		String text = checkItalic(TEST_TEXT);
+		text = checkBold(text);
+		text = checkUnderline(text);
+		return "<html>" + text + "</html>";
+	}
+
+	private String checkItalic(String string) {
+		if (italicBox.isSelected()) {
+			return "<i>" + string + "</i>";
+		}
+		return string;
+	}
+
+	private String checkBold(String string) {
+		if (boldBox.isSelected()) {
+			return "<b>" + string + "</b>";
+		}
+		return string;
+	}
+
+	private String checkUnderline(String string) {
+		if (underlineBox.isSelected()) {
+			return "<u>" + string + "</u>";
+		}
+		return string;
 	}
 
 	public static void main(String[] args) {
