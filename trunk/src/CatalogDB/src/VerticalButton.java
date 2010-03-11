63ec07c2f02c9cf0ade23a037ed4b5561ab26ace
bond-forever@yandex.ru <bond-forever@yandex.ru@6423568c-d02b-11de-83be-cfe0592a616e>
2010-03-11 13:14:31 +0000

    Изменен исходный код класса VerticalButton.

--- trunk/src/CatalogDB/src/VerticalButton.java
+++ trunk/src/CatalogDB/src/VerticalButton.java
@@ -1,64 +1,57 @@
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Insets;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JToggleButton;
 
-public class VerticalButton extends JButton {
-
-	private String caption;
-	private boolean clockwise;
-
-	public VerticalButton(String caption, boolean clockwise) {
-		this.caption = caption;
-		this.clockwise = clockwise;
-	}
-
-	public void paint(Graphics g) {
-		super.paint(g);
-
-		Font f = getFont();
-		FontMetrics fm = getFontMetrics(f);
-		int captionHeight = fm.getHeight();
-		int captionWidth = fm.stringWidth(caption);
-
-		BufferedImage bi = new BufferedImage(captionHeight + 4,
-				captionWidth + 4, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2 = (Graphics2D) bi.getGraphics();
-
-		g2.setColor(new Color(0, 0, 0, 0));
-		g2.fillRect(0, 0, bi.getWidth(), bi.getHeight());
-
-		g2.setColor(getForeground());
-		g2.setFont(f);
-
-		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-		if (clockwise) {
-			g2.rotate(Math.PI / 2);
-		} else {
-			g2.rotate(-Math.PI / 2);
-			g2.translate(-bi.getHeight(), bi.getWidth());
-		}
-
-		g2.drawString(caption, 2, -6);
-
-		Icon icon = new ImageIcon(bi);
-		setIcon(icon);
-
-		setMargin(new Insets(15, 2, 15, 2));
-		setActionCommand(caption);
-	}
-
-	public void setText(String text) {
-		this.caption = text;
-	}
+class VerticalButton extends JButton {
+   
+   XButton template;
+   boolean clockwise;
+   
+   VerticalButton(String text, boolean clockwise) {
+      template = new XButton(text, getFont());
+      this.clockwise = clockwise;
+      
+      Dimension d = template.getPreferredSize();
+      setPreferredSize(new Dimension(d.height, d.width));
+   }
+   
+   public void setFont(Font font) {
+	   super.setFont(font);
+	   if(template != null) {
+		   template.setFont(font);		   
+	   }	   
+   }
+   
+   protected void paintComponent(Graphics g) {
+      Graphics2D g2 = (Graphics2D) g.create();
+      
+      Dimension d = getSize();
+      template.setSize(d.height, d.width);
+      
+      if (clockwise) {
+         g2.rotate(Math.PI / 2.0);
+         g2.translate(0, -getSize().width);
+      } else {
+         g2.translate(0, getSize().height);
+         g2.rotate(- Math.PI / 2.0);
+      }
+      template.setSelected(this.getModel().isPressed());
+      template.paintComponent(g2);
+      g2.dispose();
+   }
+   
+   private class XButton extends JToggleButton {
+      XButton(String text, Font font) {
+         super(text);
+         setFont(font);
+      }
+      
+      public void paintComponent(Graphics g) {
+         super.paintComponent(g);
+      }
+   }
 }
