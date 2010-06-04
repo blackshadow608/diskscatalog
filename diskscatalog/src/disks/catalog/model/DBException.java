7045d5f41dde46058110c830403e33b6b12b4b1a
bond-forever <bond-forever@yandex.ru>
2010-06-04 14:25:09 +0000

    Добавлены комментарии к классу DBException.

--- diskscatalog/src/disks/catalog/model/DBException.java
+++ diskscatalog/src/disks/catalog/model/DBException.java
@@ -1,18 +1,33 @@
 package disks.catalog.model;
 
 /**
- * Представляет собой исключительную ситуацию или исключение, которое было выброшено в процессе
- * выполнения операций с БД.
+ * Представляет собой исключительную ситуацию или исключение, которое было
+ * выброшено в процессе выполнения операций с БД.
  * 
  * @author Alexander Levin
  */
 public class DBException extends Exception {
 
+	/**
+	 * Создает новый объект {@link DBException}, который будет содержать
+	 * текстовое {@link String} сообщение.
+	 * 
+	 * @param message
+	 *            - {@link String} сообщение, которое будет содержать объект.
+	 */
 	public DBException(String message) {
-		super(message);		
+		super(message);
 	}
-	
-	public DBException(Exception e) {
-		super(e);
+
+	/**
+	 * Создает новый объект {@link DBException} на основе родительского
+	 * исключения.
+	 * 
+	 * @param exception
+	 *            - исключение {@link Exception}, на основе которого создается
+	 *            новый объект.
+	 */
+	public DBException(Exception exception) {
+		super(exception);
 	}
 }
